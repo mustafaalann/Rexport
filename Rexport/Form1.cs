b8377c68e0dcfd06575e9beebad5c6521cbf9da9
Berk Hoşgören <berk.2000hosgoren@hotmail.com>
2021-01-03 17:16:08 +0300

    deneme

--- Rexport/Form1.cs
+++ Rexport/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Rexport
 {
-    public partial class Form1 : Form
+    public partial class Home : Form
     {
-        public Form1()
+        public Home()
         {
             InitializeComponent();
         }
