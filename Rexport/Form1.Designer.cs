ea4439a42f4975639d23edfa36c638d594d3acaa
Berk Hoşgören <berk.2000hosgoren@hotmail.com>
2021-01-03 17:48:18 +0300

    hey

--- Rexport/Form1.Designer.cs
+++ Rexport/Form1.Designer.cs
@@ -54,18 +54,16 @@ namespace Rexport
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(281, 710);
+            this.panel1.Size = new System.Drawing.Size(211, 577);
             this.panel1.TabIndex = 0;
             // 
             // createButton
             // 
             this.createButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.createButton.Location = new System.Drawing.Point(0, 383);
-            this.createButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.createButton.Location = new System.Drawing.Point(0, 311);
             this.createButton.Name = "createButton";
-            this.createButton.Size = new System.Drawing.Size(281, 60);
+            this.createButton.Size = new System.Drawing.Size(211, 49);
             this.createButton.TabIndex = 3;
             this.createButton.Text = "Create";
             this.createButton.UseVisualStyleBackColor = true;
@@ -73,10 +71,9 @@ namespace Rexport
             // editButton
             // 
             this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.editButton.Location = new System.Drawing.Point(0, 315);
-            this.editButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.editButton.Location = new System.Drawing.Point(0, 256);
             this.editButton.Name = "editButton";
-            this.editButton.Size = new System.Drawing.Size(281, 60);
+            this.editButton.Size = new System.Drawing.Size(211, 49);
             this.editButton.TabIndex = 2;
             this.editButton.Text = "Edit";
             this.editButton.UseVisualStyleBackColor = true;
@@ -85,10 +82,9 @@ namespace Rexport
             // homeButton
             // 
             this.homeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.homeButton.Location = new System.Drawing.Point(0, 247);
-            this.homeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.homeButton.Location = new System.Drawing.Point(0, 201);
             this.homeButton.Name = "homeButton";
-            this.homeButton.Size = new System.Drawing.Size(281, 60);
+            this.homeButton.Size = new System.Drawing.Size(211, 49);
             this.homeButton.TabIndex = 1;
             this.homeButton.Text = "Home";
             this.homeButton.UseVisualStyleBackColor = true;
@@ -100,9 +96,8 @@ namespace Rexport
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(281, 254);
+            this.panel2.Size = new System.Drawing.Size(211, 206);
             this.panel2.TabIndex = 0;
             // 
             // label1
@@ -110,10 +105,9 @@ namespace Rexport
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(4, 182);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(3, 148);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 29);
+            this.label1.Size = new System.Drawing.Size(200, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Welcome to Rexport";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -121,10 +115,9 @@ namespace Rexport
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Rexport.Properties.Resources.Rexport;
-            this.pictureBox1.Location = new System.Drawing.Point(-65, -14);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(-49, -11);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(391, 225);
+            this.pictureBox1.Size = new System.Drawing.Size(293, 183);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -134,20 +127,18 @@ namespace Rexport
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(143)))), ((int)(((byte)(143)))));
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(281, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(211, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1296, 38);
+            this.panel3.Size = new System.Drawing.Size(972, 31);
             this.panel3.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(400, 120);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(300, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1109, 91);
+            this.label2.Size = new System.Drawing.Size(893, 73);
             this.label2.TabIndex = 2;
             this.label2.Text = "DENEME aşlckşlsakcşlasşcas";
             this.label2.Click += new System.EventHandler(this.label2_Click_1);
@@ -156,10 +147,9 @@ namespace Rexport
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(383, 247);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(287, 201);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1109, 91);
+            this.label3.Size = new System.Drawing.Size(893, 73);
             this.label3.TabIndex = 3;
             this.label3.Text = "DENEME aşlckşlsakcşlasşcas";
             // 
@@ -167,26 +157,26 @@ namespace Rexport
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(364, 374);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(273, 304);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(1109, 91);
+            this.label4.Size = new System.Drawing.Size(125, 73);
             this.label4.TabIndex = 4;
-            this.label4.Text = "DENEME aşlckşlsakcşlasşcas";
+            this.label4.Text = "nbr";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1577, 710);
+            this.ClientSize = new System.Drawing.Size(1183, 577);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
--- Rexport/Form1.cs
+++ Rexport/Form1.cs
@@ -47,5 +47,10 @@ namespace Rexport
 
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
